e866e509585a68c0fad70944d3085f88774cb2d8
IGAP <gatadeabhijit@gmail.com>
2022-10-22 16:58:43 +0530

    first commit

---
+++ models/Course.js
@@ -0,0 +1,79 @@
+var Database = require("./Database");
+
+class Course {
+  constructor() {
+    this.id = 0;
+    this.trainerid = 0;
+    this.name = "";
+    this.description = "";
+    this.imagepath = "";
+
+    this.query = "";
+    this.db = new Database();
+  }
+
+  save = () => {
+    this.query =
+      "INSERT INTO courses(trainerid, name, description, imagepath) ";
+    this.query +=
+      "VALUES(" +
+      this.trainerid +
+      ", '" +
+      this.name +
+      "', '" +
+      this.description +
+      "', '" + this.imagepath + "')";
+    return new Promise((resolve, reject) => {
+      this.db.query(this.query, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+
+  list = () => {
+    this.query = "SELECT * FROM courses WHERE trainerid = " + this.trainerid + " ORDER BY name";
+    return new Promise((resolve, reject) => {
+      this.db.query(this.query, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+
+  get = () => {
+    this.query = "SELECT * FROM courses WHERE id = " + this.id;
+    return new Promise((resolve, reject) => {
+      this.db.query(this.query, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+
+  update = () => {
+    this.query = "UPDATE courses ";
+    this.query += "SET name = '" + this.name + "', ";
+    this.query += "description = '" + this.description + "' ";
+    this.query += "WHERE id = " + this.id;
+    return new Promise((resolve, reject) => {
+      this.db.query(this.query, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+
+  delete = () => {
+    this.query = "DELETE FROM courses WHERE id = " + this.id;
+    return new Promise((resolve, reject) => {
+      this.db.query(this.query, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+}
+
+
+module.exports = Course;
